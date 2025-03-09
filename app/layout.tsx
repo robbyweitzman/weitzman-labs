3c992999d70a7d3ab83739c50d9da89996e64cfd
robbyweitzman <robby.weitzman@gmail.com>
2025-03-09 13:46:44 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 
 export const metadata: Metadata = {
   title: "Weitzman Labs",
